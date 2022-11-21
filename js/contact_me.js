76b45504d2056e8d70f4bce5ce610d2f2a565f33
dmd-designs <35509661+dmd-designs@users.noreply.github.com>
2022-11-21 21:34:31 +0000

    Deploying to gh-pages from  @ b63d04fd9b7f4dddb0ea849d92045f4d79e3d861 🚀

---
+++ js/contact_me.js
@@ -0,0 +1,14 @@
+$(function() {
+
+  $("#contactForm input,#contactForm textarea").jqBootstrapValidation();
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+  $('#success').html('');
+});
